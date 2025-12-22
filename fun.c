10ee5a641ecabcf3268f5fae49f23ceb3cf58a1b
GiulioRengu <giuliorengucci@gmail.com>
2025-12-22 20:26:49 +0100

    3

--- fun.c
+++ fun.c
@@ -1,12 +1,26 @@
-#include "fun.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "fun.h"
 
-user* createNewUser(char* id, unsigned port, unsigned pass){
+/*
+    Crea un nuovo utente (user) che ha id, password e porta UDP.
+ * 
+@param id Nome dell'utente.
+@param password password dell'utente.
+@param port numero della porta UDP dell'utente.
+
+@return Un puntatore ad un nuovo oggetto user allocato.
+ */
+user* createNewUser(char* id, unsigned password, unsigned port){
     user* ret = malloc(sizeof(user));
-    ret->id = id;
-    ret->port = port;
-    ret->password = pass;
-    ret->listOfFriends = NULL;
+    if(ret==NULL) return NULL;
+
+    ret->id=malloc(strlen(id)+1);
+    strcpy(ret->id, id);
+    ret->port=port;
+    ret->password=password;
+    ret->listOfFriends=NULL;
     return ret;
 }
 
