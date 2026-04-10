07a2b34b129e3a0bb408980358a2ded87416fe21
GiulioRengu <giuliorengucci@gmail.com>
2026-04-10 10:08:19 +0200

    minor changes

--- fun.c
+++ fun.c
@@ -5,19 +5,18 @@
 
 #include "fun.h"
 
-
 int checkValidData(char* id, unsigned port, unsigned password)
 {
-    if(id==NULL || id[0]=='\0')
+    if(id==NULL || id[0]=='\0') //da fare il controllo SUBITO non puoi mettere numeri come nome utente 
         return 1;
+        
+        for(int i=0; id[i]!='\0'; i++)
+            if(!isalnum(id[i]))
+                return 1;
 
-    if(strlen(id)>=8)
+    if(strlen(id)>=8)    
         return 1;
-
-    for(int i=0; id[i]!='\0'; i++)
-        if(!isalnum(id[i]))
-            return 1;
-
+    
 
     if(port==0 || port>9999)
         return 2;
